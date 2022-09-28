fefcb66fc001539c42590811f9d2931101c3aded
shahroze Gondal <gondalshahroze@gmail.com>
2022-09-28 21:13:53 +0500

    employee & assets crud created

--- assets/admin.py
+++ assets/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+import assets.models
 # Register your models here.
+
+admin.site.register(assets.models.Asset)
+admin.site.register(assets.models.AssignedAsset)
--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from .serializers import DepartmentSerializer, EmployeeSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework import viewsets
+from employees.models import Employee, Department
 # Create your views here.
+
+
+class DepartmentViewSet(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
