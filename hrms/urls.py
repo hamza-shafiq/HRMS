10d9ca0802f8eb394f5d13f75bf0599c5f72fdf1
Hamza Shafique <hamzashafique054@gmail.com>
2022-10-03 22:46:37 +0500

    static files setup

--- hrms/urls.py
+++ hrms/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,4 +39,5 @@ urlpatterns = [
         description="API for all the basic functionalities of HRMS",
         version="1.0.0"
     ), name='openapi-schema')
-]
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
