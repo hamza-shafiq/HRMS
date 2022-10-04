89e0ee4080a7c58c42d7d44b010b79dd528fd3e2
Kamran Virk <kamranv1rk312@gmail.com>
2022-10-04 17:36:59 +0500

    fix

--- hrms/settings.py
+++ hrms/settings.py
@@ -113,8 +113,12 @@ WSGI_APPLICATION = 'hrms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': env('DATABASE_ENGINE'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
     }
 }
 
