437ba73a14101fb43483b32f0dc1fdd53a954585
Hamza Shafique <hamzashafique054@gmail.com>
2022-10-04 18:01:16 +0500

    github actions

--- assets/tests.py
+++ assets/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from models import Asset
+
+
+class AssetTests(TestCase):
+    def test_asset_is_created_successfully(self):
+        asset = Asset(
+            asset_model='2018',
+            asset_type='Mac',
+            cost=200000
+        )
+        asset.save()
