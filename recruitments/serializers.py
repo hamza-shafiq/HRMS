375606ac54c6b8cee4b92072d6ef04da88233e68
shahroze Gondal <gondalshahroze@gmail.com>
2022-10-05 19:18:47 +0500

    Recruit fixes

--- recruitments/serializers.py
+++ recruitments/serializers.py
@@ -1,28 +1,30 @@
 from django.http import JsonResponse
-from rest_framework import serializers
-from rest_framework.response import Response
-
+from rest_framework import serializers, status
 from employees.models import Employee
 from recruitments.models import Recruits, Referrals
 
 
 class RecruitsSerializer(serializers.HyperlinkedModelSerializer):
+    referrers = serializers.CharField(required=False)
+
     class Meta:
         model = Recruits
-        fields = '__all__'
+        fields = ['url', 'id', 'first_name', 'last_name', 'email', 'phone_number', 'position', 'resume', 'status', 'referrers']
 
     def create(self, validated_data):
-        if self.initial_data.get('referrers'):
+        if self.initial_data.get('referrers') is not None:
             try:
-                emp = Employee.objects.filter(id=self.initial_data.get('referrers'))
-                if emp.first():
-                    recruits = Recruits.objects.create(**validated_data)
-                    Referrals.objects.create(referer=emp.first(), recruit=recruits)
-                    return recruits
+                emp = Employee.objects.filter(id=self.initial_data.get('referrers')).first()
+                if emp:
+                    validated_data.pop('referrers')
+                    recruit = Recruits.objects.create(**validated_data)
+                    Referrals.objects.create(referer=emp, recruit=recruit)
+                    return recruit
                 raise serializers.ValidationError(
                     self.default_error_messages['Employee with this referrer id does not exist'])
             except Exception as e:
                 raise serializers.ValidationError(
-                    self.default_error_messages['Invalid Referrer id'])
-        raise serializers.ValidationError(
-                    self.default_error_messages['Referrer id is not provided'])
+                    self.default_error_messages['Invalid referrer id'])
+        recruits = Recruits.objects.create(**validated_data)
+        return recruits
+
