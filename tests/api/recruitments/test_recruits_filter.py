fda1e75c64cc211ea0d85e869915cb9309f17e7e
shahroze Gondal <gondalshahroze@gmail.com>
2022-11-03 22:38:23 +0500

    -a

--- tests/api/recruitments/test_recruits_filter.py
+++ tests/api/recruitments/test_recruits_filter.py
@@ -18,6 +18,7 @@ def test_filter_recruits_does_not_exist(admin_factory, authed_token_client_gener
     client = authed_token_client_generator(user)
     response = client.get(reverse('recruits-list') + "?recruit_id=" + str(user.id))
     assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json()['error'] == f'Recruit with id: {user.id} does not exist'
 
 
 def test_filter_recruits_invalid_id(admin_factory, authed_token_client_generator):
