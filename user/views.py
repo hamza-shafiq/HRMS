a5c25606c1e8e101b8af5d0278f1a06b351518b1
TheUsmanMirza <usmanbashir.mirza@gmail.com>
2023-05-30 13:58:26 +0500

    tox

--- user/views.py
+++ user/views.py
@@ -2,7 +2,6 @@ import jwt
 from django.conf import settings
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.contrib.sites.shortcuts import get_current_site
-from django.core.mail import EmailMessage
 from django.urls import reverse
 from django.utils.encoding import smart_bytes, smart_str
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
