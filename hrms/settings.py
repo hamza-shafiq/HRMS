499c8fca09817125d7073dd830b93a2568c8a656
TheUsmanMirza <usmanbashir.mirza@gmail.com>
2023-05-30 13:51:57 +0500

    update settings.py

--- hrms/settings.py
+++ hrms/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-CLIENT_URL = env('CLIENT_URL')
+CLIENT_URL = os.environ.get('CLIENT_URL', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
