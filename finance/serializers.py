05e97daa838c7c23979d7e07fc6c3e491209c977
zainshahbaz786 <zainjatt.zj@gmail.com>
2023-06-05 15:13:59 +0500

    Test Cases working

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,10 +1,5 @@
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
-from django.template.loader import render_to_string
-from employees.models import Employee
 from finance.models import Payroll
-from user.tasks import send_email
-
 
 
 class PayRollSerializer(serializers.ModelSerializer):
@@ -13,52 +8,7 @@ class PayRollSerializer(serializers.ModelSerializer):
         fields = ["id", "basic_salary", "bonus", "reimbursement", "travel_allowance", "tax_deduction", "month", "year",
                   "released", "employee"]
 
-    def create(self, validated_data):
-        employee = validated_data.get('employee')
-        if employee:
-            email_body = 'Hi ' + employee.username + '!\n' + \
-                         ' Your payroll has been generated for the month of ' + validated_data['month']\
-                         + ' ' + validated_data['year'] + '\n' + \
-                         'You can now view it at your dashboard.'
-            data = {'email_body': email_body, 'to_email': employee.email,
-                    'email_subject': 'Payroll Generated'}
-            # generate_and_send_employee_credentials(data)
-            send_email.delay(data)
-            validated_data['employee'] = employee
-            return super().create(validated_data)
-        else:
-            raise ValidationError('This asset is already assigned to someone')
-
     def to_representation(self, instance):
         ret = super(PayRollSerializer, self).to_representation(instance)
         ret['employee_name'] = str(instance.employee.get_full_name)
         return ret
-
-
-    def update(self, instance, validated_data):
-        user_email = validated_data['employee'].email
-        context = {
-            'employee': validated_data['employee'],
-            'month': validated_data['month'],
-            'year': validated_data['year'],
-            'released': validated_data['released'],
-            'basic_salary': validated_data['basic_salary']
-        }
-
-        if validated_data['released'] == instance.released:
-            email_subject = 'Payroll Released'
-
-        else:
-            email_subject = 'Payroll Created'
-        template_file_path = "emails/salary_slip.html"
-
-        email_body = render_to_string(template_file_path, context)
-        #
-        data = {
-            'email_body': email_body,
-            'to_email': user_email,
-            'email_subject': email_subject
-        }
-        send_email(data)
-        super().update(instance, validated_data)
-        return instance
--- user/views.py
+++ user/views.py
@@ -45,10 +45,6 @@ class RegisterView(generics.GenericAPIView):
                 'email_subject': 'Verify your email'}
 
         send_email.delay(data)
-        email = EmailMessage(
-            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-        email.send()
-
         return Response(user_data, status=status.HTTP_201_CREATED)
 
 
@@ -105,11 +101,6 @@ class RequestPasswordResetEmail(generics.GenericAPIView):
             data = {'email_body': email_body, 'to_email': user.email,
                     'email_subject': 'Reset your password'}
             send_email.delay(data)
-
-            # # send email
-            # email = EmailMessage(
-            #     subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-            # email.send()
             return Response({'success': 'We have sent you a link to reset your password'},
                             status=status.HTTP_200_OK)
         return Response({'error': 'Email does not exist'}, status=status.HTTP_404_NOT_FOUND)
