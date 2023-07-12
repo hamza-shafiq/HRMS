48167af1308dbb8baa07650b7c4588622359bcd2
kamran virk <kamranv1rk312@gmail.com>
2023-07-12 16:42:50 +0500

    date format fixed

--- attendance/serializers.py
+++ attendance/serializers.py
@@ -15,7 +15,7 @@ class AttendanceSerializer(serializers.ModelSerializer):
         ret = super(AttendanceSerializer, self).to_representation(instance)
         ret['employee_name'] = str(str(instance.employee.first_name).capitalize() + " " +
                                    str(instance.employee.last_name).capitalize())
-        dt = datetime.strptime(str(instance.check_in), '%Y-%m-%d %H:%M:%S.%f+00:00')
+        dt = datetime.strptime(str(instance.check_in), '%Y-%m-%d %H:%M:%S%z')
         ret['time_check_in'] = str(dt.hour + 5).zfill(2) + ":" + str(dt.minute).zfill(2) + ":" + str(dt.second).zfill(2)
         ret['check_in_date'] = dt.date()
 
@@ -24,7 +24,7 @@ class AttendanceSerializer(serializers.ModelSerializer):
         if instance.check_out is None or instance.check_out is False:
             pass
         else:
-            dt = datetime.strptime(str(instance.check_out), '%Y-%m-%d %H:%M:%S.%f+00:00')
+            dt = datetime.strptime(str(instance.check_out), '%Y-%m-%d %H:%M:%S%z')
             ret['time_check_out'] = str(dt.hour + 5).zfill(2) + ":" + str(dt.minute).zfill(2) + ":" + str(
                 dt.second).zfill(2)
 
