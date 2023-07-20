c9ee11a88f8cd3f2bfa1d41a7c31e81ce674c5dc
kamran virk <kamranv1rk312@gmail.com>
2023-07-20 16:40:18 +0500

    config added in payroll serializer

--- finance/serializers.py
+++ finance/serializers.py
@@ -5,8 +5,8 @@ from finance.models import Payroll
 class PayRollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payroll
-        fields = ["id", "basic_salary", "bonus", "reimbursement", "travel_allowance", "tax_deduction", "month", "year",
-                  "released", "employee"]
+        fields = ["id", "basic_salary", "bonus", "reimbursement", "travel_allowance", "tax_deduction", "config",
+                  "month", "year", "released", "employee"]
 
     def to_representation(self, instance):
         ret = super(PayRollSerializer, self).to_representation(instance)
