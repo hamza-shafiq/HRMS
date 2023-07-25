35d45b810a799a7ede28370448960bfd5a79bfb3
kamran virk <kamranv1rk312@gmail.com>
2023-07-25 19:40:04 +0500

    added modified by&at

--- policies/models.py
+++ policies/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from user.models import BaseModel
+from employees.models import Employee
 
 
 class Policies(BaseModel):
     file_name = models.CharField(max_length=100)
-    policy_file = models.FileField(upload_to='media', verbose_name="resume pdf")
+    policy_file = models.FileField(upload_to='media', verbose_name="policy_pdf")
+    modified_by = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name="policies")
 
     class Meta:
         db_table = "policies"
