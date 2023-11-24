9bc2db6495dca2ed7217a840067d2d6d6a16a696
SanaaYousaf <sanayousaf012@gmail.com>
2023-11-24 19:50:21 +0500

    improve test_cases

--- tests/api/finance/test_payroll_details.py
+++ tests/api/finance/test_payroll_details.py
@@ -1,4 +1,3 @@
-from django.core import mail
 from django.db.models import signals
 from django.urls import reverse
 from factory.django import mute_signals
@@ -8,7 +7,7 @@ from finance.models import Payroll
 
 
 def test_payroll_update(admin_factory, employee_factory, payroll_factory,
-                        authed_token_client_generator, mailoutbox, celery_eager_run_on_commit):
+                        authed_token_client_generator, celery_eager_run_on_commit):
     with mute_signals(signals.post_save):
         employee = employee_factory(email='employee@gmail.com')
         admin_user = admin_factory()
@@ -25,7 +24,3 @@ def test_payroll_update(admin_factory, employee_factory, payroll_factory,
     response = client.put(reverse('payroll-detail', kwargs={'pk': payroll.id}), data=data, format='json')
     assert response.status_code == status.HTTP_200_OK
     assert Payroll.objects.filter(id=response.data['id']).exists()
-    assert len(mail.outbox) == 1  # Assuming only one email is sent
-    email = mail.outbox[0]
-    assert email.subject == 'Payroll Released'
-    assert email.to == [employee.email]
