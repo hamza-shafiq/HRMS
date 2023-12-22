8a9b9927dc4ba7ca351616da8442415b2b9ed49f
SanaaYousaf <sanayousaf012@gmail.com>
2023-12-22 14:03:29 +0500

    Admin_attendance_page pagination

---
+++ hrms/pagination.py
@@ -0,0 +1,12 @@
+from rest_framework.pagination import PageNumberPagination
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+
+    def get_page_size(self, request):
+        page_size = request.query_params.get('page_size', None)
+
+        if page_size:
+            return int(page_size)
+
+        return super().get_page_size(request)
