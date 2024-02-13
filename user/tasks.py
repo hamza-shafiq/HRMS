de1aca4246fe43a6ffe2580952cca646391f2ef3
Muhammd Usman Bashir <48524353+TheUsmanMirza@users.noreply.github.com>
2024-02-13 19:08:20 +0500

    Added email_from for payroll (#83)

--- user/tasks.py
+++ user/tasks.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from celery import shared_task
 from django.core.mail import EmailMessage
 
@@ -5,5 +6,5 @@ from django.core.mail import EmailMessage
 @shared_task()
 def send_email(data):
     email = EmailMessage(
-        subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
+        subject=data['email_subject'], body=data['email_body'], from_email=settings.EMAIL_FROM, to=[data['to_email']])
     email.send()
