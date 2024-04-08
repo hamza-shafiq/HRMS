c6fecaf7f36ef18f73a5b825600a208c1d308ca1
SanaaYousaf <sanayousaf012@gmail.com>
2024-04-08 13:34:24 +0500

    add my profile tab

--- employees/permissions.py
+++ employees/permissions.py
@@ -11,7 +11,7 @@ class EmployeePermission(BaseCustomPermission):
     def has_permission(self, request, view):
         user_role = check_user_role(request.user)
         if view.action == 'employee_detail':
-            if user_role == UserRoles.EMPLOYEE:
+            if user_role == UserRoles.EMPLOYEE or (user_role == UserRoles.ADMIN and request.user.is_employee):
                 return True
         return super().has_permission(request, view)
 
