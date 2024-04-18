fa26d55a1af95c3c91a81d21c1e0f0cd821e3e79
Muhammd Usman Bashir <48524353+TheUsmanMirza@users.noreply.github.com>
2024-04-18 15:34:39 +0500

    Admin can change password from admin panel (#93)

--- user/admin.py
+++ user/admin.py
@@ -6,5 +6,12 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'is_verified', 'is_staff')
 
+    def save_model(self, request, obj, form, change):
+        field = 'password'
+        super().save_model(request, obj, form, change)
+        if change and field in form.changed_data and form.cleaned_data.get(field):
+            obj.set_password(obj.password)
+            obj.save()
+
 
 admin.site.register(User, UserAdmin)
