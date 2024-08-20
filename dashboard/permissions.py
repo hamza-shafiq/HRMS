db6bbfb41ff009cdebf2bb07612c18cc2c749758
UsmanCh009 <usmandotmumtaz@gmail.com>
2024-08-20 15:22:20 +0500

    fix dashboard permissions check

--- dashboard/permissions.py
+++ dashboard/permissions.py
@@ -8,10 +8,9 @@ class DashboardPermission(BaseCustomPermission):
         user = request.user
         if not user.is_admin and user.is_employee:
             employee = Employee.objects.get(id=user.id)
-            if view.action == 'employee_dashboard':
-                if not employee.is_team_lead or (employee.is_team_lead and 'team_lead_dashboard' not in request.path):
-                    return True
             if view.action == 'team_lead_dashboard':
                 if employee.is_team_lead:
                     return True
+            elif view.action == 'employee_dashboard':
+                return True
         return super().has_permission(request, view)
